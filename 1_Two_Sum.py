b5f73df95e88da0ba03bd9bc3bd0604e518f4038
LEVE1012 <151630636+LEVE1012@users.noreply.github.com>
2024-11-20 20:23:36 +0800

    Update 1_Two_Sum.py

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -13,4 +13,5 @@ class Solution(object):
                 return [num_to_index[targetNum], i]
             num_to_index[num] = i
 
-#这道题采用了哈希表的方式，在遍历的过程中顺便构建哈希表，最后将查找出的结果打印出来，时间复杂度为O(n)，空间复杂度为O(n)
+#这道题采用了哈希表的方式，在遍历的过程中顺便构建哈希表，最后将查找出的结果打印出来
+#时间复杂度为O(n)，空间复杂度为O(n)
