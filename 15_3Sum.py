e3853a024cfff8529aa398dfeef1c8f1a5050571
LEVE1012 <151630636+LEVE1012@users.noreply.github.com>
2024-11-27 16:29:16 +0800

    Create 15_3Sum.py

---
+++ 15_3Sum.py
@@ -0,0 +1,37 @@
+#Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+#Notice that the solution set must not contain duplicate triplets.
+
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        n = len(nums)
+        result = []
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+        return result
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+#采用双指针的方法，对数组中的每个元素进行遍历，再用双指针从左到右寻找需要的数，保证不会遗漏
+#时间O(n2)，空间O(1)
