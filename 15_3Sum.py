5e4e40b6bce2891656a0f5e18cac55901a60ef00
LEVE1012 <151630636+LEVE1012@users.noreply.github.com>
2024-11-27 18:35:49 +0800

    Update 15_3Sum.py

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -19,9 +19,9 @@ class Solution(object):
                     left += 1
                     right -= 1
 
-                    while left < right and nums[left] == nums[left - 1]:
+                    while left < right and nums[left] == nums[left + 1]:
                         left += 1
-                    while left < right and nums[right] == nums[right + 1]:
+                    while left < right and nums[right] == nums[right - 1]:
                         right -= 1
                 elif total < 0:
                     left += 1
